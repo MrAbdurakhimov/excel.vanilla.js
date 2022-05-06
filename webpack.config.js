84b97a0bd79b441b09590a863641f0448ae47b7a
Mukhammadyusuf Abdurakhimov <Mukhammadyusuf_Abdurakhimov@epam.com>
2022-05-07 02:31:38 +0500

    eslint fix

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,19 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
 const filename = (ext) => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  ];
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
+};
 console.log('isProd: ', isProd);
 console.log('isDev: ', isDev);
 
@@ -52,27 +65,12 @@ module.exports = {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: isDev,
-              reloadAll: true,
-            },
-          },
-          'css-loader',
-          'sass-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        use: jsLoaders(),
       },
     ],
   },
