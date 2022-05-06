cb2eac288a996663402d57c589bdf6c0ac20d80f
Mukhammadyusuf Abdurakhimov <Mukhammadyusuf_Abdurakhimov@epam.com>
2022-05-07 02:34:29 +0500

    finish webpack config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import './module';
 import './scss/index.scss';
-
-async function start() {
-  return await Promise.resolve('async working');
-}
-
-start().then(console.log);
 
 console.log('Hello World');
